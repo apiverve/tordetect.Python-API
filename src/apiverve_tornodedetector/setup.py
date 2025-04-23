4c3923195cfb3182acea1f51f186e15482122183
APIVerve Team <hello@apiverve.com>
2025-04-23 11:27:24 -0500

    Bug Fixes

---
+++ src/apiverve_tornodedetector/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='apiverve_tornodedetector',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    description='Tor Detect is an API that checks whether a given IP address belongs to a known Tor exit node. Useful for security, access control, and analytics.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
